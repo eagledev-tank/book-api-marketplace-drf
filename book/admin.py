6eeb3752ab6e63b60b68d7adabf0765c3bd14ab1
eagledev_tank <nurislomlapasov@gmail.com>
2024-10-09 15:13:35 +0500

    book yaratishda rasm qo'shish hammasi bir joyda bo'ldi admin panelda

--- book/admin.py
+++ book/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from book.models import Book, BookImage
 
 
-@admin.register(Book)
+class BookImageInline(admin.StackedInline):
+    model = BookImage
+    extra = 1
+
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price', 'stock', 'publication_date', 'is_in_stock')
     search_fields = ('title', 'author')
     list_filter = ('author', 'publication_date')
+    inlines = [BookImageInline]
 
 
-admin.site.register(BookImage)
+admin.site.register(Book, BookAdmin)
